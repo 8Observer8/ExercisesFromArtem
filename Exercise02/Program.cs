726f8e403c0f1a7a03120d49a166af75e5c6b286
Enzhaev Ivan <8observer8@gmail.com>
2015-06-19 00:20:15 +0300

    Initial commit

---
+++ Exercise02/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Exercise02
+{
+    /// <summary>
+    /// Задание 2. Вывести времена года в зависимости от месяца введенного пользователем.
+    /// Conditional Statement <if>
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Спрашиваем пользователя: какой текущий месяц?
+
+            Console.Write("What is the current month?  ");
+
+            // Присваиваем переменной полученное значение
+
+            string month = Console.ReadLine();
+
+            if (month == "December" || month == "January" || month == "Febrary")
+            {
+                Console.WriteLine("It is winter");
+            }
+
+
+            else if (month == "March" || month == "April" || month == "May")
+            {
+                Console.WriteLine("It is spring");
+            }
+
+
+            else if (month == "June" || month == "July" || month == "August")
+            {
+                Console.WriteLine("It is summer");
+            }
+
+
+            else if (month == "September" || month == "October" || month == "November")
+            {
+                Console.WriteLine("It is autumn");
+            }
+
+
+            else
+            {
+                Console.WriteLine("Provide appropriate input");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ TestsExercise01/TestsExercise01.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise01;
+
+namespace TestsExercise01
+{
+    [TestClass]
+    public class TestsExercise01
+    {
+        [TestMethod]
+        public void TestJanuary()
+        {
+            string actual = Program.GetNameOfMonth(1);
+            string expected = "January";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestFebruary()
+        {
+            string actual = Program.GetNameOfMonth(2);
+            string expected = "February";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMarch()
+        {
+            string actual = Program.GetNameOfMonth(3);
+            string expected = "March";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestApril()
+        {
+            string actual = Program.GetNameOfMonth(4);
+            string expected = "April";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMay()
+        {
+            string actual = Program.GetNameOfMonth(5);
+            string expected = "May";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestJune()
+        {
+            string actual = Program.GetNameOfMonth(6);
+            string expected = "June";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestJuly()
+        {
+            string actual = Program.GetNameOfMonth(7);
+            string expected = "July";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestAugust()
+        {
+            string actual = Program.GetNameOfMonth(8);
+            string expected = "August";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSeptember()
+        {
+            string actual = Program.GetNameOfMonth(9);
+            string expected = "September";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestOctober()
+        {
+            string actual = Program.GetNameOfMonth(10);
+            string expected = "October";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestNovember()
+        {
+            string actual = Program.GetNameOfMonth(11);
+            string expected = "November";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestDecember()
+        {
+            string actual = Program.GetNameOfMonth(12);
+            string expected = "December";
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
